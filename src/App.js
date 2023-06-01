bcf67d79d5499f76ebfeeb8caa9656be153e2bb2
saikrishna-12 <saikrishnasai12@gmail.com>
2023-06-01 10:58:08 +0530

    adding my profile and a card intro to project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Navbar';
+import MyIntro from './MyIntro';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <MyIntro/>
       
     </div>
   );
