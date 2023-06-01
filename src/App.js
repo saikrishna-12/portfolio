fa72e4a8fe3c3a68d3c7a659523cdccb38b0fe8f
saikrishna-12 <saikrishnasai12@gmail.com>
2023-06-02 01:27:03 +0530

    cards of service and other details

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Navbar from './Navbar';
 import MyIntro from './MyIntro';
 
+
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <MyIntro/>
-      
+            
     </div>
   );
 }
